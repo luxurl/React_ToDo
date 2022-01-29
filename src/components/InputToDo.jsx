2a0aee8a27b317d1f6a7bac524d16e4b357396fa
luxurl <78976230+luxurl@users.noreply.github.com>
2022-01-29 21:07:56 +0900

    CSS-JS inputToDo

--- src/components/InputToDo.jsx
+++ src/components/InputToDo.jsx
@@ -5,9 +5,18 @@ export const InputTodo = (props) => {
   //propsを分割代入
   const { todoText, onchange, onClick } = props;
 
+  const style = {
+    backgroundColor: "#c1ffff",
+    width: "400px",
+    height: "30px",
+    padding: "8px",
+    margin: "8px",
+    borderRadius: "8px"
+  };
+
   return (
     <>
-      <div className="input-area ">
+      <div style={style}>
         {/* これだけだと常に初期値が空のtodoTextが入るだけになってしまう
          <input placeholder="TODOを入力" value={todoText}/> 
          
