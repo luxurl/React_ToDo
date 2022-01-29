94e0fac59ba9f814ead8e02dd70e58d390dc08d0
luxurl <78976230+luxurl@users.noreply.github.com>
2022-01-29 18:00:52 +0900

    構造をまず作成する

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,35 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のToDo</p>
+        <ul>
+          <div>
+            <li>aaaa</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>bbbbbb</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+        <p>完了のToDo</p>
+        <ul>
+          <div>
+            <li>aaaa</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+      <div></div>
+    </>
+  );
 };
